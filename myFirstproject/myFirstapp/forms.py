e5c13062b6549490ec8fd689fe70df0579604e84
manikumar-codes <72206341+manikumar-codes@users.noreply.github.com>
2020-12-21 10:28:37 +0530

    Initial commit

---
+++ myFirstproject/myFirstapp/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class Feedbackform(forms.Form):
+    title = forms.CharField(label='Title',max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
+    subject = forms.CharField(label='Subject Description',max_length=200,widget=forms.Textarea(attrs={'class':'form-control'}))
+    email = forms.CharField(label='Email',max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
+
